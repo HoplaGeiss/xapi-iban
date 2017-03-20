949cc8605fdd17ad96ceaa414566909be00c3dcd
Gabriel Muller <gabriel.muller.12@gmail.com>
2017-03-20 10:13:25 +0000

    back to dummy call

--- src/app/components/iban/iban.service.ts
+++ src/app/components/iban/iban.service.ts
@@ -13,11 +13,11 @@ export class IbanService {
   constructor(private http: Http) {}
 
   search(iban: String): Promise<any> {
-    const url =  'https://discovery-server-xapi.cfapps.io/iban/';
-    // const url =  'api/banks';
+    // const url =  'https://discovery-server-xapi.cfapps.io/iban/';
+    const url =  'api/banks';
     return this.http
-      .get(`${url}${iban}`)
-      // .get(`${url}/1`)
+      // .get(`${url}${iban}`)
+      .get(`${url}/1`)
       .toPromise()
       .then(this.extractData)
       .catch(this.handleError);
